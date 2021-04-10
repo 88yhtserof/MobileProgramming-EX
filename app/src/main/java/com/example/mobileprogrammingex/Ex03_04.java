594e5718a97cbad9a807879a3915b01a944747fe
Lim YunHwi <88yhtserof@naver.com>
2021-04-10 15:30:18 +0900

    03_04 Android Studio와 github 연동

---
+++ app/src/main/java/com/example/mobileprogrammingex/Ex03_04.java
@@ -0,0 +1,14 @@
+package com.example.mobileprogrammingex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex03_04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex03_04);
+    }
+}
